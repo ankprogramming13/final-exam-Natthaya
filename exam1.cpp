052e91d4f7637e3dced7dc177297946b93b63859
Unknown <natthayasofia@gmail.com>
2018-02-22 11:51:25 +0700

    1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+using namespace std;
+int main()
+{
+int Number1, Number2;
+char e;
+// input two number
+cout << "Enter first number : ";
+cin >> Number1;
+cout << "Enter second number : ";
+cin >> Number2;
+do
+{
 
-int main() {
 
-        return 0;
+// Diplay two add
+cout << endl;
+cout << "Sum " << Number1 << " + " << Number2;
+cout << " = " << Number1 + Number2 << endl;
+cout << endl;
+cout << "Sum " << Number1 << " - " << Number2;
+cout << " = " << Number1 - Number2 << endl;
+cout << endl;
+cout << "Sum " << Number1 << " * " << Number2;
+cout << " = " << Number1 * Number2 << endl;
+cout << endl;
+cout << "Sum " << Number1 << " / " << Number2;
+cout << " = " << Number1 / Number2 << endl;
+cout << "Enter first number : ";
+cin >> Number1;
+cout << "Enter second number : ";
+cin >> Number2;
+}
+while(e!='q');
+cout<<"\n###END Pocess###\t"<<endl;
+return (0);
 }
